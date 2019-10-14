f967ab925746f93cafe910f5ab462370b245d9d2
Samuli Seppänen <samuli@openvpn.net>
2019-10-14 15:14:12 +0300

    Support updating the inventory
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- manifests/controller.pp
+++ manifests/controller.pp
@@ -2,12 +2,25 @@
 #
 # Configure a Puppet Bolt controller node
 #
+# @param user
+#   The local user that is used for outbound SSH connections.
+# @param inventory_template_source
+#   Inventory template file for Puppet Bolt. The inventory will be populated by "bolt-inventory-pdb".
 # @example
 #   include bolt::controller
 #
 class bolt::controller
 (
-  String $user
+  String           $user,
+  String           $puppetdb_url,
+  String           $inventory_template_source,
+  String           $bolt_inventory_pdb,
+  String           $cacert,
+  String           $cert,
+  String           $key,
+  Integer          $hourly_inventory_updates,
+  String           $cron_email,
+  Optional[String] $inventory = undef,
 )
 {
 
@@ -17,5 +30,24 @@ class bolt::controller
   $homedir = $user ? {
     'root'  => '/root',
     default => "/home/${user}"
+  }
+
+  $inventory_file = $inventory ? {
+    undef   => "${homedir}/inventory.yaml",
+    default => $inventory,
+  }
+
+  cron { 'update-bolt-inventory-from-puppetdb':
+    command     => "${bolt_inventory_pdb} ${inventory_template_source} --cacert ${cacert} --cert ${cert} --key ${key} --url ${puppetdb_url} -o ${inventory_file}",
+    user        => 'root',
+    minute      => "*/${hourly_inventory_updates}",
+    environment => [ 'PATH=/bin:/usr/bin:/usr/local/bin:/opt/puppetlabs/bin', "MAILTO=${cron_email}" ],
+  }
+
+  file { $inventory_file:
+    ensure => 'present',
+    owner  => $user,
+    group  => $user,
+    mode   => '0600',
   }
 }
