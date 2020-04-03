179a31e0ab4e40006ae115bc9383a27a551edabe
Samuli Seppänen <samuli@openvpn.net>
2020-04-03 11:11:33 +0300

    Add proper unit tests and fix metadata
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- spec/classes/controller_spec.rb
+++ spec/classes/controller_spec.rb
@@ -4,6 +4,9 @@ describe 'bolt::controller' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
+      let(:params) { {  'user'                    => 'bolt',
+                        'puppetdb_url'            => 'https://puppet.example.org:8081',
+                        'inventory_template_path' => '/home/bolt/inventory-template.yaml', } }
 
       it { is_expected.to compile }
     end
